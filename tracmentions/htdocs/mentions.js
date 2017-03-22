fb5efcb81996a59f2a5b4cd59975495b8ad64f9c
Ryan J Ollos <ryanollos@verasonics.com>
2017-03-21 18:37:20 -0700

    0.2dev: Fix `TypeError` creating a new ticket with mention
    Remove use of `ITemplateStreamFilter` and use JavaScript
    instead.

--- tracmentions/htdocs/mentions.js
+++ tracmentions/htdocs/mentions.js
@@ -1,12 +1,11 @@
 jQuery(function ($) {
-  $('[data-trac-mentions = all]').mentionsInput({
+  $('.wikitext').mentionsInput({
     elastic: false,
     onDataRequest: function (mode, query, callback) {
       var data = UserList;
       data = _.filter(data, function (item) {
         return item.name.toLowerCase().indexOf(query.toLowerCase()) > -1
       });
-
       callback.call(this, data);
     },
     resetInput: false
--- tracmentions/web_ui.py
+++ tracmentions/web_ui.py
@@ -3,12 +3,9 @@
 
 import re
 
-from genshi.core import START
-from genshi.filters.transform import Transformer
-
 from trac.config import Option
 from trac.core import Component, implements
-from trac.web.api import IRequestFilter, ITemplateStreamFilter
+from trac.web.api import IRequestFilter
 from trac.web.chrome import Chrome, ITemplateProvider, add_script, \
                             add_script_data, add_stylesheet
 from trac.web.href import Href
@@ -18,7 +15,7 @@ class MentionsModule(Component):
     """Autocomplete @<Username> or #<tag>. Parse any ticket description or
     comment text to leverage hashtags as Trac tags
     """
-    implements(IRequestFilter, ITemplateProvider, ITemplateStreamFilter)
+    implements(IRequestFilter, ITemplateProvider)
 
     underscore_location = Option('trac', 'underscorejs_location',
         'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/',
@@ -37,62 +34,6 @@ class MentionsModule(Component):
     def get_templates_dirs(self):
         return []
 
-    # ITemplateStreamFilter methods
-
-    def filter_stream(self, req, method, filename, stream, data):
-        """Setup user and tags autocomplete"""
-
-        if method == 'xhtml':
-            return stream.filter(lambda s: self._process_stream(req, s))
-        return stream
-
-    # Protected methods
-
-    def _process_stream(self, req, stream):
-        """Filter stream to install user / hashtag autocomplete if any
-        of the following is found
-
-          - textarea.wikitext
-          - .ticket input#field-owner , .ticket input#field-reporter (users)
-        """
-        # Pre-process stream to match known targets
-        stream = stream | \
-                 Transformer('//textarea[contains(@class, "wikitext")]') \
-                     .attr('data-trac-mentions', 'all').end()
-        # Add the users data to page
-        users = []
-        for i, (username, name, email) in enumerate(
-                self.env.get_known_users()):
-            user_object = {
-                'id': (username or ''),
-                'value': '@' + (username or ''),
-                'name': (name or '') + ' (' + (username or '') + ')',
-                'avatar': '/contacts/' + (username or '') + '.gif',
-                'type': 'contact'
-            }
-            users.append(user_object)
-
-        add_script_data(req, {'UserList': users})
-
-        # Add js and css files
-        not_found = True
-        for kind, data, pos in stream:
-            if kind == START and not_found:
-                autocomplete = data[1].get('data-trac-mentions')
-                if autocomplete:
-                    not_found = False
-                    if self.underscore_location:
-                        add_script(req,
-                                   self.underscore_href('underscore-min.js'))
-                        add_stylesheet(req,
-                                       'mentions/jquery.mentionsInput.css')
-                        add_script(req, 'mentions/jquery.mentionsInput.js')
-                        add_script(req, 'mentions/mentions.js')
-                    else:
-                        self.log.warning("MentionsModule can't work because "
-                                         "of missing configuration")
-            yield kind, data, pos
-
     # IRequestFilter Methods
 
     def pre_process_request(self, req, handler):
@@ -101,8 +42,11 @@ class MentionsModule(Component):
         description = req.args.get('field_description')
         if comment or description:
             # Parse the wiki-formatted fields to get the mentioned users
-            comment_users = re.findall('(?<=@)[\w]+', comment)
-            description_users = re.findall('(?<=@)[\w]+', description)
+            comment_users = description_users = []
+            if comment:
+                comment_users = re.findall('(?<=@)[\w]+', comment)
+            if description:
+                description_users = re.findall('(?<=@)[\w]+', description)
             mentioned_users = set(comment_users) | set(description_users)
 
             # Create the value for field_cc
@@ -120,7 +64,25 @@ class MentionsModule(Component):
         return handler
 
     def post_process_request(self, req, template, data, content_type):
-        if data:
+        if template == 'ticket.html':
+            users = [{
+                'id': (username or ''),
+                'value': '@' + (username or ''),
+                'name': (name or '') + ' (' + (username or '') + ')',
+                'avatar': '/contacts/' + (username or '') + '.gif',
+                'type': 'contact'
+            } for username, name, email in self.env.get_known_users()]
+            add_script_data(req, {'UserList': users})
+
+            if self.underscore_location:
+                add_script(req, self.underscore_href('underscore-min.js'))
+                add_script(req, 'mentions/jquery.mentionsInput.js')
+                add_stylesheet(req, 'mentions/jquery.mentionsInput.css')
+                add_script(req, 'mentions/mentions.js')
+            else:
+                self.log.warning("MentionsModule can't work because of "
+                                 "missing configuration")
+
             # Read the fields that support wiki formatting
             comment = req.args.get('comment')
             description = req.args.get('field_description')
